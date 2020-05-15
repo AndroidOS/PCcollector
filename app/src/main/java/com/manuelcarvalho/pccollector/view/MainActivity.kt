82346eadd573b006ab0d415d015105d28ddeed92
AndroidOS <burrowsgit@gmail.com>
2020-05-16 06:46:52 +1000

    Added bulk data method (1/2)

--- app/src/main/java/com/manuelcarvalho/pccollector/view/MainActivity.kt
+++ app/src/main/java/com/manuelcarvalho/pccollector/view/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.manuelcarvalho.pccollector.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.manuelcarvalho.pccollector.R
+import com.manuelcarvalho.pccollector.model.Part
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +19,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            bulkData()
         }
     }
 
@@ -34,6 +36,44 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun bulkData() {
+        var cartList: List<Part>? = null
+        var manufacturer = ""
+        var cartName = ""
+        var yearMan = ""
+        try {
+            val inputStream: InputStream = assets.open("c64cartList.txt")
+            val text = inputStream.bufferedReader().use { it.readText() }
+
+            val lines = text.split("\n")
+
+            for (line in lines) {
+
+                if (line[0] == '*') {
+
+                    manufacturer = line.substringAfter('*')
+                    //Log.d(TAG,"$manufacturer")
+                } else {
+                    cartName = line.substringBefore(',')
+                    if (line.contains(',')) {
+                        yearMan = line.substringAfter(',')
+                    } else {
+                        yearMan = "???"
+                    }
+
+                    Log.d(TAG, "$yearMan")
+                }
+
+            }
+
+
+            //cartList = splitString(a)
+
+        } catch (e: Exception) {
+            Log.d(TAG, e.toString())
         }
     }
 }
