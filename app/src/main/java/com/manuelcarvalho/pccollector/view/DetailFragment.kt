6330d9298ebaa3dead788348e2d129b29c7eb3d3
AndroidOS <burrowsgit@gmail.com>
2020-05-22 09:14:09 +1000

    Added WebView

--- app/src/main/java/com/manuelcarvalho/pccollector/view/DetailFragment.kt
+++ app/src/main/java/com/manuelcarvalho/pccollector/view/DetailFragment.kt
@@ -1,15 +1,21 @@
 package com.manuelcarvalho.pccollector.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.manuelcarvalho.pccollector.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 private const val TAG = "DetailFragment"
 
 class DetailFragment : Fragment() {
+
+    private lateinit var cart: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,30 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.title = "Web View"
+
+        arguments?.let {
+            cart = DetailFragmentArgs.fromBundle(it).search
+        }
+
+
+        //        val url = "https://www.google.com/search?q=%22Commodore%22+%22VIC-20%22"
+        val url = "https://www.google.com/search?q=Commodore64+${cart}"
+        Log.d(TAG, "WEBf $url")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+
+        webView.loadUrl(url)
     }
 
 }
